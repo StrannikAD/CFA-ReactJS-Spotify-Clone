75065f69bae307fe5db2763bdc5da493fcd1f275
Anna <a.dimitrova.au@gmail.com>
2017-05-20 14:25:06 +1000

    Added basic search bar functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar/SearchBar';
 
 export default class App extends Component {
 
@@ -6,13 +7,19 @@ export default class App extends Component {
     super(props);
     this.state = {
       initialMessage: 'greeting',
+      song: '',
     };
+  }
+
+  fetchSongs = () => {
+    console.log('hey I\'m bring clicked');
   }
 
   render() {
     const { initialMessage } = this.state;
     return (
       <div>
+        <SearchBar updateText={(song) => this.setState({ song })} fetchSongs={this.fetchSongs} />
         {initialMessage}
       </div>
     );
